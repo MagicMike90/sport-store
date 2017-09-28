cd834b79ffa8e3597451b0d7a25ac465a99ee00f
Michael Luo <mluo@echoice.com.au>
2017-09-28 12:02:07 +1000

    add checkout and detail page

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,11 +1,14 @@
 import { Component } from "@angular/core";
 import { Product } from "../model/product.model";
 import { ProductRepository } from "../model/product.repository";
+import { Cart } from "../model/cart.model";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "store",
   moduleId: module.id,
-  templateUrl: "store.component.html"
+  templateUrl: "store.component.html",
+  styleUrls: ['./store.component.css']
 })
 
 export class StoreComponent {
@@ -13,7 +16,9 @@ export class StoreComponent {
   public productsPerPage = 4;
   public selectedPage = 1;
 
-  constructor(private repository: ProductRepository) { }
+  constructor(private repository: ProductRepository,
+    private cart: Cart,
+    private router: Router) { }
 
   get products(): Product[] {
     let pageIndex = (this.selectedPage - 1) * this.productsPerPage
@@ -41,10 +46,9 @@ export class StoreComponent {
     return Math.ceil(this.repository
       .getProducts(this.selectedCategory).length / this.productsPerPage)
   }
-  
-  get pageNumbers(): number[] {
-    return Array(Math.ceil(this.repository
-      .getProducts(this.selectedCategory).length / this.productsPerPage))
-      .fill(0).map((x, i) => i + 1);
+
+  addProductToCart(product: Product) {
+    this.cart.addLine(product);
+    this.router.navigateByUrl("/cart");
   }
 }
